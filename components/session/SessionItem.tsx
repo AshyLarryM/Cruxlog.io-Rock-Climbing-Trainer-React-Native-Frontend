dce15e03a2c73a3ee9f4e4d256b140bedbac1336
AshyLarryM <LarryM093@gmail.com>
2024-11-15 13:57:53 -0800

    Open more stats

--- components/session/SessionItem.tsx
+++ components/session/SessionItem.tsx
@@ -1,30 +1,40 @@
+import React, { useState } from "react";
 import { Session } from "@/lib/utils/models/sessionModels";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, LayoutAnimation, Platform, UIManager } from "react-native";
 
 interface SessionItemProps {
     session: Session;
 }
 
+if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 export function SessionItem({ session }: SessionItemProps) {
     const router = useRouter();
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
     function navigateToSession() {
         router.push({
-            pathname: '/(auth)/history/[sessionId]',
-            params:
-            {
+            pathname: "/(auth)/history/[sessionId]",
+            params: {
                 sessionId: session.id.toString(),
-                sessionName: session.sessionName || 'Unnamed Session'
+                sessionName: session.sessionName || "Unnamed Session",
             },
         });
-    };
+    }
+
+    function toggleMoreStats() {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setIsExpanded((prev) => !prev);
+    }
 
     return (
         <TouchableOpacity onPress={navigateToSession}>
             <View style={styles.sessionContainer}>
-                <Text style={styles.sessionName}>{session.sessionName || 'Unnamed Session'}</Text>
+                <Text style={styles.sessionName}>{session.sessionName || "Unnamed Session"}</Text>
                 <Text style={styles.sessionDate}>
                     {new Date(session.createdAt).toLocaleDateString()}
                 </Text>
@@ -48,10 +58,8 @@ export function SessionItem({ session }: SessionItemProps) {
                             <Text style={styles.verticalSeparator}>|</Text>
                         )}
 
-
                     {session.sessionStats.highestRouteGrade !== null && (
-                        <View style={styles.column}
-                        >
+                        <View style={styles.column}>
                             <Text style={styles.highlightHeader}>Hardest Route</Text>
                             <Text style={styles.hardestRouteText}>
                                 {session.sessionStats.highestRouteGrade}
@@ -71,33 +79,43 @@ export function SessionItem({ session }: SessionItemProps) {
                                 Total: {session.sessionStats.totalSends || "0"}
                             </Text>
                             <Text style={styles.highlightsText}>
-                                Flash: {session.sessionStats.totalFlashes || "0"}<Ionicons name="flash" size={20} color="#ffcc00" />
+                                Flash: {session.sessionStats.totalFlashes || "0"}{" "}
+                                <Ionicons name="flash" size={20} color="#ffcc00" />
                             </Text>
                         </View>
                     </View>
                 </View>
 
-                <View style={styles.statsContainer}>
-                    <Text style={styles.statsHeader}>Session Stats:</Text>
-                    {session.sessionStats.highestBoulderGrade !== null && (
-                        <Text>Highest Boulder Grade: {session.sessionStats.highestBoulderGrade}</Text>
-                    )}
+                {/* More Button */}
+                <TouchableOpacity onPress={toggleMoreStats} style={styles.moreButton}>
+                    <Text style={styles.moreText}>
+                        {isExpanded ? <Ionicons name="chevron-up" size={16} /> : <Ionicons name="chevron-down" size={16} />}
+                    </Text>
+                </TouchableOpacity>
 
-                    {session.sessionStats.highestRouteGrade !== null && (
-                        <Text>Highest Route Grade: {session.sessionStats.highestRouteGrade}</Text>
-                    )}
-                    <Text>Total Climbs: {session.sessionStats.totalClimbs}</Text>
+                {/* Conditional Stats Section */}
+                {isExpanded && (
+                    <View style={styles.statsContainer}>
+                        <Text style={styles.statsHeader}>Session Stats:</Text>
+                        {session.sessionStats.highestBoulderGrade !== null && (
+                            <Text>Highest Boulder Grade: {session.sessionStats.highestBoulderGrade}</Text>
+                        )}
 
-                    <Text>Total Attempts: {session.sessionStats.totalAttempts}</Text>
-                    {session.sessionStats.completedBoulders > 0 && (
-                        <Text>Completed Boulders: {session.sessionStats.completedBoulders}</Text>
-                    )}
-                    {session.sessionStats.completedRoutes > 0 && (
-                        <Text>Completed Routes: {session.sessionStats.completedRoutes}</Text>
-                    )}
-                    <Text>Total Sends: {session.sessionStats.totalSends}</Text>
-                    <Text>Total Flashes: {session.sessionStats.totalFlashes}</Text>
-                </View>
+                        {session.sessionStats.highestRouteGrade !== null && (
+                            <Text>Highest Route Grade: {session.sessionStats.highestRouteGrade}</Text>
+                        )}
+                        <Text>Total Climbs: {session.sessionStats.totalClimbs}</Text>
+                        <Text>Total Attempts: {session.sessionStats.totalAttempts}</Text>
+                        {session.sessionStats.completedBoulders > 0 && (
+                            <Text>Completed Boulders: {session.sessionStats.completedBoulders}</Text>
+                        )}
+                        {session.sessionStats.completedRoutes > 0 && (
+                            <Text>Completed Routes: {session.sessionStats.completedRoutes}</Text>
+                        )}
+                        <Text>Total Sends: {session.sessionStats.totalSends}</Text>
+                        <Text>Total Flashes: {session.sessionStats.totalFlashes}</Text>
+                    </View>
+                )}
             </View>
         </TouchableOpacity>
     );
@@ -107,59 +125,58 @@ const styles = StyleSheet.create({
     sessionContainer: {
         marginBottom: 24,
         padding: 16,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderRadius: 8,
-        shadowColor: '#000',
+        shadowColor: "#000",
         shadowOpacity: 0.1,
         shadowRadius: 4,
     },
     sessionName: {
         fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
+        fontWeight: "bold",
+        textAlign: "center",
     },
     sessionDate: {
         fontSize: 14,
-        color: '#666',
+        color: "#666",
         marginBottom: 8,
-        textAlign: 'center',
+        textAlign: "center",
     },
     separator: {
         marginVertical: 8,
-        borderBottomColor: '#ccc',
+        borderBottomColor: "#ccc",
         borderBottomWidth: 1,
-        width: '100%',
+        width: "100%",
     },
     verticalSeparator: {
         width: 1,
-        backgroundColor: '#ccc',
+        backgroundColor: "#ccc",
         marginHorizontal: 4,
-        alignSelf: 'stretch',
+        alignSelf: "stretch",
     },
-
     intensityContainer: {
-        padding: 8
+        padding: 8,
     },
     statsContainer: {
         marginTop: 12,
     },
     hardestBoulderText: {
         paddingTop: 4,
-        textAlign: 'center',
-        fontWeight: 'bold',
+        textAlign: "center",
+        fontWeight: "bold",
         fontSize: 28,
         color: "#6c47ff",
     },
     hardestRouteText: {
         paddingTop: 4,
-        textAlign: 'center',
-        fontWeight: 'bold',
+        textAlign: "center",
+        fontWeight: "bold",
         fontSize: 28,
         color: "#DE8A43",
     },
     statsHeader: {
         fontSize: 16,
-        fontWeight: 'bold',
+        fontWeight: "bold",
         marginBottom: 4,
     },
     highlightsContainer: {
@@ -171,21 +188,32 @@ const styles = StyleSheet.create({
         borderColor: "#ccc",
         borderRadius: 8,
         width: "100%",
-        shadowColor: "#000"
+        shadowColor: "#000",
     },
     highlightsText: {
-        textAlign: 'left',
-        fontWeight: '600',
+        textAlign: "left",
+        fontWeight: "600",
         paddingTop: 2,
     },
     column: {
         flex: 1,
-        alignItems: 'center',
-        padding: 8
+        alignItems: "center",
+        padding: 8,
     },
     highlightHeader: {
         fontWeight: "bold",
         marginBottom: 4,
-        textAlign: 'center',
+        textAlign: "center",
     },
-})
+    moreButton: {
+        marginTop: 12,
+        alignSelf: "center",
+        padding: 8,
+        backgroundColor: "#ebebeb",
+        borderRadius: 20
+    },
+    moreText: {
+        fontSize: 16,
+        color: "#6c47ff",
+    },
+});
